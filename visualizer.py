7bfd1195d2afa5d257465ed71ad992b580ea4be2
Yoann FOREAU <yforeau@e3r11p8.42.fr>
2019-12-10 17:25:23 +0100

    Init stack_a

--- visualizer.py
+++ visualizer.py
@@ -18,13 +18,13 @@ def get_input():
 
 class Visu:
     def __init__(self, numbers):
+        # game data
         self.instructions = self.launch_push_swap(numbers)
         self.stack_a = []
         for s in numbers:
             self.stack_a.append(int(s))
-        print(self.instructions)
-        print(self.stack_a)
         self.stack_b = []
+        # window data
         self.win = Tk()
         self.win_h = 800
         self.win_w = 1000
@@ -32,15 +32,17 @@ class Visu:
         self.win.geometry(str(self.win_w) + "x" + str(self.win_h) + "+"\
             + str(x) + "+" + str(y))
         self.win.resizable(width=False, height=False)
-        self.left_frame = Frame(self.win, bg="black")
-        self.right_frame = Frame(self.win, bg="white")
-        self.left_frame.pack(side=LEFT, expand=True, fill=BOTH)
-        self.right_frame.pack(side=RIGHT, expand=True, fill=BOTH)
-
-    def get_start_pos(self):
-        x = (self.win.winfo_screenwidth() / 2) - (self.win_w / 2)
-        y = (self.win.winfo_screenheight() / 2) - (self.win_h / 2)
-        return int(x), int(y)
+        # widgets
+        self.left_canvas = Canvas(self.win, bg="black")
+        self.right_canvas = Canvas(self.win, bg="white")
+        self.left_canvas.pack(side=LEFT, expand=True, fill=BOTH)
+        self.right_canvas.pack(side=RIGHT, expand=True, fill=BOTH)
+        # visu data
+        self.quit = False
+        self.step = 0
+        self.stack_a_obj, self.stack_b_obj = self.init_stacks()
+        # launch mainf
+        self.win.after(0, self.mainf)
 
     def launch_push_swap(self, numbers):
         inst = subprocess.check_output([PUSH_SWAP_PATH] + numbers,
@@ -52,14 +54,41 @@ class Visu:
                 exit(1)
         return inst
 
+    def get_start_pos(self):
+        x = (self.win.winfo_screenwidth() / 2) - (self.win_w / 2)
+        y = (self.win.winfo_screenheight() / 2) - (self.win_h / 2)
+        return int(x), int(y)
+
+    def init_stacks(self):
+        len_a = len(self.stack_a)
+        if len_a == 0:
+            return None, None
+        stack_a_obj = []
+        sorted_stack_a = sorted(self.stack_a)
+        h = 30 if 800 / len_a > 30 else 800 / len_a
+        w = 300 / len_a
+        y = 0
+        for n in self.stack_a:
+            x1, y1 = 0, y * h
+            x2, y2 = (sorted_stack_a.index(n) + 1) * w, y1 + h
+            bar = self.left_canvas.create_rectangle(x1, y1, x2, y2, fill="white")
+            stack_a_obj.append(bar)
+            y += 1
+        return stack_a_obj, None
+
     def checker(self):
         if self.stack_b != None:
             return False
         return self.stack_a == sorted(self.stack_a)
 
+    def mainf(self):
+        #self.async_actions()
+        if self.quit == True:
+            self.quit = False
+        else:
+            self.win.after(1, self.mainf)
+
 
 numbers = get_input()
-#if numbers == None:
-#    exit(1)
 v = Visu(numbers)
 v.win.mainloop()
